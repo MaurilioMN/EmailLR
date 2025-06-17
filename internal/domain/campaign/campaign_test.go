3e774cd37a301f08630e28d22bb11801b87341a1
MaurilioMN <magalhaes.neto713@gmail.com>
2025-06-17 12:46:49 -0300

    Implementando Testify

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,27 +1,37 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // TestNewCampaign verifica se a função NewCampaign cria corretamente uma nova campanha.
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 
-	// Dados de entrada simulados
+	// Dados de entrada simulados: Arrange
 	name := "CampaignX"
 	content := "Body"
-	contact := []string{"email@e.com","email2@e.com"}
+	contact := []string{"email@e.com", "email2@e.com"}
 
-	//Criação da campanha
+	//Criação da campanha: ACT
 	campaign := NewCampaign(name, content, contact)
 
 	//Verifica se as variaveis está correto (esperado dados de entrada simulado)
-	if campaign.ID != "1" {
-		t.Errorf("Expected ID")
-	} else if campaign.Name != name {
-		t.Errorf("Expected correct name")
-	} else if campaign.Content != content {
-		t.Errorf("Expected correct c")
-	} else if len(campaign.Contact)!= len(contact) {
-		t.Errorf("Expected correct Email")
-	}
+	// if campaign.ID != "1" {
+	// 	t.Errorf("Expected ID")
+	// } else if campaign.Name != name {
+	// 	t.Errorf("Expected correct name")
+	// } else if campaign.Content != content {
+	// 	t.Errorf("Expected correct c")
+	// } else if len(campaign.Contact) != len(contact) {
+	// 	t.Errorf("Expected correct Email")
+	// }
 
+	//Testify Para simplificar a verificação acima: Assert
+	assert.Equal(campaign.ID, "1")
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contact), len(contact))
 }
